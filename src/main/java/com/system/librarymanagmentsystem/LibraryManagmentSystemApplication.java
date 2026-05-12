a52027439fa61fba8f39fcd93ba9206ae3b07273
Omar Saad <omarisadev@gmail.com>
2026-05-12 10:31:38 +0300

    feat: add CommandLineRunner bean to initialize default HeadAdmin account on startup

--- src/main/java/com/system/librarymanagmentsystem/LibraryManagmentSystemApplication.java
+++ src/main/java/com/system/librarymanagmentsystem/LibraryManagmentSystemApplication.java
@@ -1,7 +1,13 @@
 package com.system.librarymanagmentsystem;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.system.librarymanagmentsystem.DAO.PersonDAO;
+import com.system.librarymanagmentsystem.app.HeadAdmin;
+import com.system.librarymanagmentsystem.app.Name;
 
 @SpringBootApplication
 public class LibraryManagmentSystemApplication {
@@ -10,4 +16,17 @@ public class LibraryManagmentSystemApplication {
         SpringApplication.run(LibraryManagmentSystemApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner initDefaultAdmin() {
+        return args -> {
+            PersonDAO dao = new PersonDAO();
+            if (dao.getRecordById("admin") == null) {
+                Name adminName = new Name("System Admin", "System", "Admin");
+                HeadAdmin defaultAdmin = HeadAdmin.register("admin", adminName, "admin");
+                dao.insertRecord(defaultAdmin);
+                System.out.println("Default HeadAdmin created (id: admin, password: admin)");
+            }
+            dao.disconnect();
+        };
+    }
 }
